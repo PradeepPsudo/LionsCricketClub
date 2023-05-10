b1bc08133f7ca2dc271f0192adddf0ab70215ffb
pradeep <pradeep_p@intuit.com>
2023-05-10 16:09:33 +0530

    package.json lockfile sync

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,13 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { Typography } from "@material-ui/core";
-import Download from "./Download";
 
 function About() {
-  const  [isDownload, setIsDownload] = useState(false);
 
-  const clickHandler =()=>{
-    setIsDownload(true);
-  }
+
   // return <Typography>About us page</Typography>;
   return <div className="page">
       <Typography className="font-colour">About us page</Typography>;
-      <button onClick={clickHandler}>download</button>
-      {isDownload && <Download/>}
+     
   </div>
 }
 export default About;
